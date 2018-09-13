edc3ac0feb551d9d58f38d4e8310f4755b1dc46a
tharakadileepa <tharakadileepa@gmail.com>
2018-09-13 09:37:29 +0530

    this my 3rd one

---
+++ src/lk/ijse/pos/dao/custom/QueryDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.ijse.pos.dao.custom;
+
+import lk.ijse.pos.dao.SuperDAO;
+
+/**
+ *
+ * @author THARAKA
+ */
+public interface QueryDAO extends SuperDAO{
+    
+}
